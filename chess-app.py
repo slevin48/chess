25a5dc085ce79ed9c50961683835ea9e002e298d
slevin48 <slevin.an209@gadz.org>
2021-02-07 18:58:44 +0100

    Stockfish score on Demand

--- chess-app.py
+++ chess-app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import requests as rq
+import matplotlib.pyplot as plt
 import chess
 import chess.pgn
 import chess.svg
@@ -13,15 +15,14 @@ game = chess.pgn.read_game(pgn)
 st.title(game.headers["White"]+" vs "+game.headers["Black"])
 # st.write(game.headers["Link"])
 
+# scorebox = st.checkbox("Display Score",False)
+
 board = game.board()
 moves = [move for move in game.mainline_moves()]
 mv = st.slider("Move",0,len(moves),len(moves))
 
 for move in moves[0:mv]:
     board.push(move)
-
-# for move in game.mainline_moves():
-#     board.push(move)
 
 def render_svg(svg):
     """Renders the given svg string."""
@@ -30,3 +31,20 @@ def render_svg(svg):
     st.write(html, unsafe_allow_html=True)
 
 render_svg(chess.svg.board(board))
+
+# if scorebox:
+#     # Send request if scorebox is ticked
+#     score = []
+#     boardScore = game.board()
+#     for move in moves[0:mv]:
+#         r = rq.post("https://stockfish48.herokuapp.com/score",json = {"data":board.fen()})
+#         res = r.json()
+#         data = res['data']
+#         score.append(data)
+#         boardScore.push(move)
+
+
+#     fig, ax = plt.subplots()
+#     ax.plot(score)
+#     st.sidebar.title("Score")
+#     st.sidebar.write(fig)
