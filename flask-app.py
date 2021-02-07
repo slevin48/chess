ff0590b6cc33aa54fcced76a40f01bd4235074cf
slevin48 <slevin.an209@gadz.org>
2021-02-07 17:52:36 +0100

    Pass port to Flask app as an env variable

--- flask-app.py
+++ flask-app.py
@@ -21,4 +21,5 @@ def score():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=8080)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
